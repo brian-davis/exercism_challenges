d7ad653a91dfc5d83acc6f9cce7b0f711e6f7a5e
Brian Davis <contact@briandavis.name>
2014-10-03 21:52:58 -0700

    minesweeper

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,25 +1,10 @@
 class Grains
-  attr_accessor :board
-
-  def initialize
-    @board = Array.new 64
-    fill_board
-  end
-
   def square n
-    # 2**(n - 1)
     fail ArgumentError if n > 64
-    board[n - 1]
+    2**(n - 1)
   end
 
   def total
-    board.reduce(:+)
-  end
-
-  private
-
-  def fill_board
-    board[0] = 1
-    1.upto(63).each { |i| board[i] = board[i - 1] * 2 }
+    (1..64).map { |n| square n }.reduce :+
   end
 end
