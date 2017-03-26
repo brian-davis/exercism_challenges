6a86f8befcf1e27efce60b71c5330aeb69acea48
Brian Davis <g_brian_davis@protonmail.com>
2017-03-26 17:59:19 -0600

    style

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,11 +1,18 @@
+// Package gigasecond calculates a time one billion seconds if the future
+// from any given time.
 package gigasecond
-import("time")
+
+import (
+	"time"
+)
 
 const testVersion = 4
 
+// AddGigasecond takes a time.Time and returns a time.Time one billion
+// seconds in the future from it.
 func AddGigasecond(t time.Time) time.Time {
-  seconds := t.Unix()
-  future_seconds := seconds + 1000000000
-  future_time := time.Unix(future_seconds, 0)
-  return future_time
+	seconds := t.Unix()
+	futureSeconds := seconds + 1000000000
+	futureTime := time.Unix(future_seconds, 0)
+	return futureTime
 }
