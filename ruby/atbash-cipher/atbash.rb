a3b8296cc9b30f3a81f1f51b70c81b73b6a898ae
Brian Davis <g_brian_davis@protonmail.com>
2018-04-24 21:29:14 -0600

    atbash refactor

--- ruby/atbash-cipher/atbash.rb
+++ ruby/atbash-cipher/atbash.rb
@@ -1,25 +1,8 @@
-class Atbash
-  ALPHABET = *'a'..'z'
-  CIPHER =  ALPHABET.reverse
+module Atbash
+  ALPHABET = ('a'..'z').to_a.join + ('0'..'9').to_a.join
+  CIPHER   = ('a'..'z').to_a.join.reverse + ('0'..'9').to_a.join
 
-  def self.encode text
-    text.gsub(/\s+/, '')
-        .chars
-        .map { |c| swap c }
-        .compact
-        .each_slice(5)
-        .map(&:join)
-        .join ' '
-  end
-
-  def self.swap c
-    c = c.downcase
-    if c.match /[0-9]/
-      c
-    elsif c.match /\W/
-      nil
-    else
-      CIPHER[ALPHABET.index c]
-    end
+  def self.encode(text)
+    text.gsub(/\W/, '').downcase.tr(ALPHABET, CIPHER).chars.each_slice(5).map(&:join).join(" ")
   end
 end
