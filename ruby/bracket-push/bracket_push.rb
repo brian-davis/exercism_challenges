32ece15ebfd008807fbe7ae977356a2aa28840aa
Brian Davis <g_brian_davis@protonmail.com>
2018-04-24 21:07:05 -0600

    bracket-push

--- ruby/bracket-push/bracket_push.rb
+++ ruby/bracket-push/bracket_push.rb
@@ -3,12 +3,11 @@ module Brackets
   RIGHT = "]})"
 
   def self.paired?(str)
-    str.chars.each.with_object([]) do |c, acc|
+    str.each_char.with_object([]) do |c, acc|
       if LEFT.include?(c)
         acc.push(c)
       elsif RIGHT.include?(c)
-        return false unless acc.last == c.tr(RIGHT, LEFT)
-        acc.pop
+        return false unless acc.pop == c.tr(RIGHT, LEFT)
       end
     end.empty?
   end
